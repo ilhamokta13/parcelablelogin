9c4314b8164b4cf7945621c6a0254dc90c8ee846
ilhamokta13 <ilhamok8@gmail.com>
2023-03-24 14:06:12 +0700

    Latihan Parcelable - KM 4

---
+++ app/src/main/java/com/example/parcelablelogin/DataModel.kt
@@ -0,0 +1,43 @@
+package com.example.parcelablelogin
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class DataModel(
+    var name: String,
+    var email: String,
+    var phone: String,
+    var address: String,
+    var age: String
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!,
+        parcel.readString()!!
+    )
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(email)
+        parcel.writeString(phone)
+        parcel.writeString(address)
+        parcel.writeString(age)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<DataModel> {
+        override fun createFromParcel(parcel: Parcel): DataModel {
+            return DataModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<DataModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
+
